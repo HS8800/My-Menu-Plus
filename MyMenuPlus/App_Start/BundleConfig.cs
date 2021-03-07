70aa23ea921de27fef017f803047e921b8a428fb
Harri Smith <harrismith988@yahoo.co.uk>
2021-03-07 19:40:59 +0000

    Added Menu Selection Page & Cleaned Bundles

--- MyMenuPlus/App_Start/BundleConfig.cs
+++ MyMenuPlus/App_Start/BundleConfig.cs
@@ -9,54 +9,84 @@ namespace MyMenuPlus
         public static void RegisterBundles(BundleCollection bundles)
         {
 
-            bundles.Add(new ScriptBundle("~/bundles/Jquery").Include(
-                    "~/Scripts/jquery-3.5.1.min.js"));
+            //Menu Editor
+            bundles.Add(new StyleBundle("~/bundles/MenuEditorPageStyles").Include(
+                    "~/Content/Menu.css",
+                    "~/Content/MainScale.css",
+                    "~/Content/SectionEditor.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/MenuScripts").Include(
-                        "~/Scripts/Scroll.js",
-                        "~/Scripts/MenuScale.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/Notifications").Include(
-                       "~/Scripts/Notifications.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/Form").Include(
-                      "~/Scripts/Form.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/MenuEditor").Include(
-                      "~/Scripts/MenuEditor.js",
-                      "~/Scripts/Currency.js"));
+            bundles.Add(new ScriptBundle("~/bundles/MenuEditorPageScripts").Include(
+                "~/Scripts/jquery-3.5.1.min.js",
+                 "~/Scripts/MenuEditor.js",
+                 "~/Scripts/Currency.js"
+             ));
 
 
+            //Menu
+            bundles.Add(new StyleBundle("~/bundles/MenuPageStyles").Include(
+                    "~/Content/Scroll.css",
+                    "~/Content/Menu.css",
+                    "~/Content/MainScale.css"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/Aos").Include(
-                    "~/Scripts/Aos.js"));
+            bundles.Add(new ScriptBundle("~/bundles/MenuPageScripts").Include(
+                 "~/Scripts/Scroll.js",
+                 "~/Scripts/MenuScale.js"
+            ));
 
-            bundles.Add(new StyleBundle("~/bundles/AosStyles").Include(
-                        "~/Content/Aos.css"));
 
-            bundles.Add(new StyleBundle("~/bundles/Home").Include(
-                        "~/Content/Home.css",
+
+            //MenuSelection
+            bundles.Add(new StyleBundle("~/bundles/MenuSelectionPageStyles").Include(
+                 "~/Content/Aos.css",
+                 "~/Content/Home.css",
+                 "~/Content/MenuSelection.css"
+             ));
+
+            bundles.Add(new ScriptBundle("~/bundles/MenuSelectionPageScripts").Include(
+             "~/Scripts/Aos.js",
+             "~/Scripts/jquery-3.5.1.min.js"
+           ));
+
+
+            //Home
+            bundles.Add(new StyleBundle("~/bundles/HomePageStyles").Include(
+               "~/Content/Home.css",
+               "~/Content/Footer.css",
+               "~/Content/Form.css",
+               "~/Content/Notifications.css",
+               "~/Content/Aos.css"
+               ));
+
+            bundles.Add(new ScriptBundle("~/bundles/HomePageScripts").Include(
+                   "~/Scripts/Aos.js",
+                   "~/Scripts/jquery-3.5.1.min.js",
+                   "~/Scripts/Form.js",
+                   "~/Scripts/Notifications.js"));
+
+
+            //Main Site Layout
+            bundles.Add(new StyleBundle("~/bundles/MainSiteLayout").Include(
                         "~/Content/Footer.css",
-                        "~/Content/Form.css",
-                        "~/Content/Notifications.css"
+                         "~/Content/Navbar.css"
                         ));
 
+            //Menu Layout
+            bundles.Add(new StyleBundle("~/bundles/MenuLayoutStyles").Include(
+                    "~/Content/Footer.css",
+                    "~/Content/MenuNavbar.css",
+                    "~/Content/Form.css",
+                    "~/Content/Brand.css",
+                    "~/Content/Notifications.css"
+                    ));
 
-            bundles.Add(new StyleBundle("~/bundles/EditorStyles").Include(
-                        "~/Content/SectionEditor.css"));
 
-            bundles.Add(new StyleBundle("~/bundles/MenuStyles").Include(
-                        "~/Content/Scroll.css",
-                        "~/Content/Menu.css",
-                        "~/Content/MainScale.css"));
+            bundles.Add(new ScriptBundle("~/bundles/MenuLayoutScripts").Include(
+                "~/Scripts/jquery-3.5.1.min.js",
+                "~/Scripts/Notifications.js",
+                "~/Scripts/Form.js"                           
+            ));
 
-            bundles.Add(new StyleBundle("~/bundles/DefaultMenuStyles").Include(
-                        "~/Content/Footer.css",
-                        "~/Content/MenuNavbar.css",
-                        "~/Content/Form.css",
-                        "~/Content/Brand.css",
-                        "~/Content/Notifications.css"
-                        ));
 
         }
     }
---
+++ MyMenuPlus/Controllers/MenuSelectionController.cs
@@ -0,0 +1,20 @@
+﻿using MyMenuPlus.Helpers;
+using MyMenuPlus.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMenuPlus.Controllers
+{
+    public class MenuSelectionController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
